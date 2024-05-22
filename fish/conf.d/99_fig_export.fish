1c27358c77283195b5aa5a4b06631058f2951611
Tobias Heide <tobias.heide@oakdigital.dk>
2024-05-22 22:33:30 +0200

    init

---
+++ fish/conf.d/99_fig_export.fish
@@ -0,0 +1,1 @@
+/Users/tobiasheidejensen/fig-export/dotfiles/dotfile.fish
---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+set -gx PATH /opt/homebrew/bin $PATH
+
+end
